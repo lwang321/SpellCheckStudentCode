bbc71dd7c832cb6fe3d13fe66e232782f79ca2e7
Lucas Wang <lucas.wang@menloschool.org>
2025-10-30 15:04:29 -0700

    Final

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -11,6 +11,28 @@ import java.util.Arrays;
 public class SpellCheck {
 
     public String[] checkWords(String[] text, String[] dictionary) {
+//        // Put given dictionary into a Trie
+//        Trie dict = new Trie();
+//        for (String word : dictionary) {
+//            dict.insert(word);
+//        }
+//
+//        // Create a Trie to track misspelled words
+//        Trie misspelledtrie = new Trie();
+//
+//        // Also need an array to track order of inserted words
+//        String[] misspelled = new String[text.length];
+//
+//        int counter = 0;
+//        for (String word : text) {
+//            // If word isn't in dictionary or in misspelledtrie, add the word to misspelledtrie and misspelled array
+//            if (!dict.lookup(word) && !misspelledtrie.lookup(word)) {
+//                misspelledtrie.insert(word);
+//                misspelled[counter] = word;
+//                counter++;
+//            }
+//        }
+
         // Put given dictionary into a TST
         TST dict = new TST();
         for (String word : dictionary) {
